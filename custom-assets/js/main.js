01c3fba90e670a5bd269e70ba8181ea532cf7fd7
adamtysonsmith <atsmith227@gmail.com>
2015-10-23 18:58:26 -0600

    Geina's first revisions for the homepage completed

--- custom-assets/js/main.js
+++ custom-assets/js/main.js
@@ -1,5 +1,11 @@
 (function($){
     $(document).ready(function(){
-
+        
+        // Toggle About Content on Homepage
+        $('.about-toggle-button').click(function(){
+            $('.about-toggle').show();
+        });
+    
+        
     });
 })(jQuery);
